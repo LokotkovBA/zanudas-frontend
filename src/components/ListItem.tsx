db792a77a8bf08cbd1879b46c495527f0d61349e
Boris Lokotkov <woweuro2010@gmail.com>
2022-09-20 11:54:43 +0300

    Indication song is copied from song list

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -12,6 +12,7 @@ const ListItem: React.FC<{ song: SongListEntry, userData: UserData  }> = ({ song
     const [songData, setSongData] = useState<SongListEntry>(song);
     const [deleteIntention, setDeleteIntention] = useState<boolean>(false);
     const [deleteButtonText, setDeleteButtonText] = useState<string>("Delete");
+    const [clickedState, setClickedState] = useState<string>('');
 
     const [curLike, setCurLike] = useState<string>(pathToThumbsUp);
 
@@ -19,11 +20,21 @@ const ListItem: React.FC<{ song: SongListEntry, userData: UserData  }> = ({ song
         navigator.clipboard.writeText(`${song.artist} - ${song.song_name}`);
     };
 
-    function toggleEditFields(){
+    function mouseDown(){
+        setClickedState('clicked');
+    };
+
+    function mouseUp(){
+        setClickedState('');
+    }
+
+    function toggleEditFields(event: React.MouseEvent<HTMLButtonElement, MouseEvent>){
+        event.stopPropagation();
         setShowEditFields(oldValue => !oldValue);
     };
 
-    function deleteItem(){
+    function deleteItem(event: React.MouseEvent<HTMLButtonElement, MouseEvent>){
+        event.stopPropagation();
         if(deleteIntention){
             postRequest('songlist/delete', '5100', `{"id": ${songData.id}}`);
             setDeleteIntention(false);
@@ -42,33 +53,36 @@ const ListItem: React.FC<{ song: SongListEntry, userData: UserData  }> = ({ song
         });
     };
 
-    function sendNewSongData(){
+    function sendNewSongData(event: React.MouseEvent<HTMLButtonElement, MouseEvent>){
+        event.stopPropagation();
         postRequest('songlist/changeall', '5100', `{"id": ${songData.id}, "artist": "${songData.artist}", "song_name": "${songData.song_name}"}`);
     };
+
+    function inputClick(event: React.MouseEvent){
+        event.stopPropagation();
+    }
 
     useEffect(() => {
         setCurLike(song.likes > 0 ? pathToThumbsUp : pathToThumbsDown);
     }, [song.likes]);
 
     return (
-        <div className="list-item">
-            {!showEditFields && <p className="song-info" onClick={copyClick}>{song.song_name}</p>}
+        <div className={`list-item ${clickedState}`} onClick={copyClick} onMouseUp={mouseUp} onMouseDown={mouseDown}>
+            {!showEditFields && <p className="song-info">{song.song_name}</p>}
             {showEditFields && userData.is_admin && 
             <>
-                <input type='text' name='artist' placeholder="artist" onChange={queueEntryChangeEvent} value={songData.artist ? songData.artist : ''} />
-                <input type='text' name='song_name' placeholder="song name" className='song-info' onChange={queueEntryChangeEvent} value={songData.song_name ? songData.song_name : ''} />
-                <button onClick={sendNewSongData}>Change</button>
+                <input type='text' name='artist' placeholder="artist" onClick={(event) => inputClick(event)} onChange={queueEntryChangeEvent} value={songData.artist ? songData.artist : ''} />
+                <input type='text' name='song_name' placeholder="song name" className='song-info' onClick={(event) => inputClick(event)} onChange={queueEntryChangeEvent} value={songData.song_name ? songData.song_name : ''} />
+                <button onClick={(event) => sendNewSongData(event)}>Change</button>
             </>}
             {(song.likes !== 0) &&
             <div className='like-text'>
                 <img src={curLike} alt='Like'/> {song.likes}
             </div>}
-            
-            {/* {song.date !== 'old' && <p className="learn-date">First played: {song.date}</p>} */}
             {userData.is_admin &&
             <>
-                <button onClick={toggleEditFields}>Edit</button>
-                <button onClick={deleteItem}>{deleteButtonText}</button>
+                <button onClick={(event) => toggleEditFields(event)}>Edit</button>
+                <button onClick={(event) => deleteItem(event)}>{deleteButtonText}</button>
             </>}
         </div>
     );

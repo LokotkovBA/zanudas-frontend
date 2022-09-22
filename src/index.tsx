146938d55f87132a39699943afc50493bff66812
Boris Lokotkov <woweuro2010@gmail.com>
2022-09-23 00:41:50 +0300

    React strict mode on entire app

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -8,9 +9,9 @@ const root = ReactDOM.createRoot(
     document.querySelector('#root') as HTMLElement
 );
 root.render(
-    // <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    // </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
